1662fc123a4f2d3bd68ec92d9279e66c30420f70
Benjamin Sherman <benjamin@bensherman.io>
2019-06-15 20:28:52 -0400

    Add basic prerequisite tree support

--- src/course.rs
+++ src/course.rs
@@ -1,5 +1,6 @@
 use std::collections::{HashSet, HashMap};
 use serde::Deserialize;
+use std::fmt;
 
 #[derive(Deserialize, PartialEq, Eq, Hash, Clone)]
 pub struct CourseID {
@@ -14,38 +15,75 @@ impl CourseID {
             code
         }
     }
+
+    pub fn from(coid: &str) -> Option<CourseID> {
+        let char_vec: Vec<char> = coid.chars().collect();
+
+        if char_vec.len() != 8 && char_vec.len() != 9 {
+            return None;
+        }
+
+        let code_str: String = char_vec[char_vec.len()-4..].iter().collect();
+
+        let code = match code_str.parse::<u16>(){
+            Ok(num) => num,
+            _ => return None
+        };
+
+        for character in &char_vec[..4] {
+            if character < &'A' || character > &'Z' {
+                return None;
+            }
+        }
+
+        let subj: String = char_vec[..4].iter().collect();
+
+        Some(CourseID { subj, code })
+    }
+}
+
+impl fmt::Display for CourseID {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{} {}", self.subj, self.code)
+    }
 }
 
 #[derive(Deserialize)]
 pub struct Course {
     name: String,
     description: String,
-    id: CourseID,
+    coid: CourseID,
     prereqs: Vec<CourseID>,
     post_options: HashSet<CourseID>,
 }
 
 impl Course {
-    pub fn new(id: &CourseID) -> Self {
+    pub fn new(coid: &CourseID) -> Self {
         Course {
             name: String::new(),
             description: String::new(),
-            id: id.clone(),
+            coid: coid.clone(),
             prereqs: Vec::new(),
             post_options: HashSet::new(),
         }
     }
 
-    pub fn getID(&self) -> CourseID {
-        self.id.clone()
+    pub fn get_id(&self) -> CourseID {
+        self.coid.clone()
     }
 
-    pub fn add_prereq(&mut self, id: &CourseID) {
-        self.prereqs.push(id.clone());
+    pub fn add_prereq(&mut self, coid: &CourseID) {
+        self.prereqs.push(coid.clone());
     }
 
-    fn add_postoption(&mut self, id: &CourseID) {
-        self.post_options.insert(id.clone());
+    fn add_postoption(&mut self, coid: &CourseID) {
+        self.post_options.insert(coid.clone());
+    }
+}
+
+impl fmt::Display for Course {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}: {}", self.coid, self.name)
     }
 }
 
@@ -59,33 +97,51 @@ impl Catalog {
     }
 
     pub fn add_course(&mut self, mut course: Course) {
-        if let Some(existing_course) = self.get_course(&course.getID()) {
+        if let Some(existing_course) = self.get_course(&course.get_id()) {
             course.post_options = existing_course.post_options.clone();
-            self.courses.remove(&course.getID());
+            self.courses.remove(&course.get_id());
         }
 
-        for id in &course.prereqs {
-            if let Some(found_course) = self.get_course_mut(&id) {
-                found_course.add_postoption(&id);
+        for coid in &course.prereqs {
+            if let Some(found_course) = self.get_course_mut(&coid) {
+                found_course.add_postoption(&coid);
             } else {
-                let mut new_course = Course::new(&id);
-                new_course.add_postoption(&id);
-                self.courses.insert(id.clone(), new_course);
+                let mut new_course = Course::new(&coid);
+                new_course.add_postoption(&coid);
+                self.courses.insert(coid.clone(), new_course);
             }
         }
 
-        self.courses.insert(course.getID(), course);
+        self.courses.insert(course.get_id(), course);
     }
 
-    pub fn emplace_course(&mut self, id: &CourseID) {
-        self.add_course(Course::new(id));
+    pub fn emplace_course(&mut self, coid: &CourseID) {
+        self.add_course(Course::new(coid));
     }
 
-    pub fn get_course(&self, id: &CourseID) -> Option<&Course> {
-        self.courses.get(id)
+    pub fn get_course(&self, coid: &CourseID) -> Option<&Course> {
+        self.courses.get(coid)
     }
 
-    pub fn get_course_mut(&mut self, id: &CourseID) -> Option<&mut Course> {
-        self.courses.get_mut(id)
+    pub fn get_course_mut(&mut self, coid: &CourseID) -> Option<&mut Course> {
+        self.courses.get_mut(coid)
+    }
+
+    pub fn get_course_tree(&self, coid: &CourseID) -> Option<Vec<&Course>> {
+        let course = match self.get_course(coid) {
+            Some(c) => c,
+            None => return None
+        };
+
+        let mut course_tree = Vec::new();
+
+        for coid in &course.prereqs {
+            let mut prereq_tree = self.get_course_tree(coid).unwrap_or_else(|| panic!("Error: Malformed catalog!"));
+            course_tree.append(&mut prereq_tree);
+        }
+
+        course_tree.push(course);
+
+        Some(course_tree)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,34 @@
-use rpi_planner::parse_catalog;
-
 use std::path::PathBuf;
 use structopt::StructOpt;
+
+use std::fs::File;
+use std::io::BufReader;
+use std::error::Error;
+
+use serde::Deserialize;
+
+mod course;
+use crate::course::*;
+
+#[derive(Deserialize)]
+struct Courses {
+    courses: Vec<Course>,
+}
+
+pub fn parse_catalog(fname: &str) -> Result<Catalog, Box<dyn Error>> {
+    let file = File::open(fname)?;
+
+    let buf_reader = BufReader::new(file);
+    let courses: Courses = serde_json::from_reader(buf_reader)?;
+
+    let mut catalog = Catalog::new();
+
+    for course in courses.courses {
+        catalog.add_course(course);
+    }
+
+    Ok(catalog)
+}
 
 /// CLI Options
 #[derive(StructOpt, Debug)]
@@ -10,6 +37,10 @@ struct Opt {
     /// Catalog file
     #[structopt(short = "c", long = "catalog", parse(from_os_str))]
     catalog: PathBuf,
+
+    /// Courses to generate prerequisites for
+    #[structopt(name = "COURSE")]
+    courses: Vec<String>
 }
 
 fn main() {
@@ -17,4 +48,26 @@ fn main() {
     let catalog_fname = opt.catalog.to_str().unwrap();
     let catalog = parse_catalog(catalog_fname)
         .unwrap_or_else(|err| panic!("Error parsing catalog: {}", err));
+
+    for input_coid in opt.courses {
+        let coid = match CourseID::from(&input_coid) {
+            Some(id) => id,
+            None => {
+                eprintln!("Error: '{}' is not in the Course ID format", input_coid);
+                continue
+            }
+        };
+        let course_tree = match catalog.get_course_tree(&coid) {
+            Some(tree) => tree,
+            None => {
+                eprintln!("Unable to generate course tree for {}", coid);
+                continue
+            }
+        };
+
+        println!("Course Tree for {}:", coid);
+        for course in &course_tree {
+            println!("\t{}", course);
+        }
+    }
 }
